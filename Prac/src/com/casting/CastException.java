38421b0ae1d94f3dad696fc784211572288c1033
PLM-AHMEDW\Admin <ahmed@pragiti.com>
2019-05-06 00:50:42 +0530

    adding

---
+++ Prac/src/com/casting/CastException.java
@@ -0,0 +1,33 @@
+package com.casting;
+
+public class CastException {
+
+	public static void main(String[] args)
+	{
+		
+		
+		
+		/*A a = new B();
+		B b = (B) a;
+		C c = (C)a;*/
+	}
+}
+
+class A
+{
+	
+}
+
+class B extends A
+{
+	
+}
+
+class C extends B
+{
+	
+}
+class D extends C
+{
+	
+}
